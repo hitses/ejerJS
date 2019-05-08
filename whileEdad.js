b5331ea364c9e46e1c3f0d3791a53fec09a38354
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-08 15:34:18 +0200

    No cogía 18 como mayor de edad arreglado

--- whileEdad.js
+++ whileEdad.js
@@ -1,7 +1,7 @@
 var edad = Math.floor(Math.random() * 100) + 1;
 
-while(edad < 18){
-  if(edad < 18){
+while(edad <= 18){
+  if(edad <= 18){
     console.log('Tienes', edad, 'años. Crece un poco más para poder continuar.');
     var edad = Math.floor(Math.random() * 100) + 1;
   }
