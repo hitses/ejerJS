faa1832908bad57f9a877da20ae744d078e2b47c
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-08 15:45:52 +0200

    Reparo do while

--- whileEdad.js
+++ whileEdad.js
@@ -1,13 +1,3 @@
-/* var edad = Math.floor(Math.random() * 100) + 1;
-
-while(edad <= 18){
-  if(edad <= 18){
-    console.log('Tienes', edad, 'años. Crece un poco más para poder continuar.');
-    edad = Math.floor(Math.random() * 100) + 1;
-  }
-}; */
-
-
 do{
   edad = Math.floor(Math.random() * 100) + 1;
   console.log(edad <= 18 ? ('Tienes ' + edad + ' años. Crece un poco más para poder continuar.') : ('Bien. Ahora que tienes ' + edad + ' años sí puedes continuar'));
