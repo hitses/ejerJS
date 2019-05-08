456d482fbd3d83cf0bb49d8257c02718f54ad9ac
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-08 15:45:33 +0200

    While convertido a do while

--- whileEdad.js
+++ whileEdad.js
@@ -1,10 +1,14 @@
-var edad = Math.floor(Math.random() * 100) + 1;
+/* var edad = Math.floor(Math.random() * 100) + 1;
 
 while(edad <= 18){
   if(edad <= 18){
     console.log('Tienes', edad, 'años. Crece un poco más para poder continuar.');
-    var edad = Math.floor(Math.random() * 100) + 1;
+    edad = Math.floor(Math.random() * 100) + 1;
   }
-};
+}; */
 
-console.log('Bien. Ahora que tienes', edad, 'años sí puedes continuar');
+
+do{
+  edad = Math.floor(Math.random() * 100) + 1;
+  console.log(edad <= 18 ? ('Tienes ' + edad + ' años. Crece un poco más para poder continuar.') : ('Bien. Ahora que tienes ' + edad + ' años sí puedes continuar'));
+}while(edad <= 18);
