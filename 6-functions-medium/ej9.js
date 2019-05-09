d94c67947cd234e31c46b97ddda0292529c16d68
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-09 12:54:49 +0200

    A ver si se arregla ya

---
+++ 6-functions-medium/ej9.js
@@ -0,0 +1,29 @@
+// crea la función extraerAcronimo
+// Esta función recibe un numero indeterminado de argumentos
+// y los recibiremos en la array input con el operador rest (...input)
+// Debe devolver un string con sus iniciales 
+// Por ejemplo:
+// extraerAcronimo('Estudiante','Del','Bootcamp','Fullstack','Geekshubs') === 'EDBFG'
+
+
+// 
+// utilizar un bucle for of
+
+// Ha de pasar los test adjuntados.
+
+let extraerAcronimo = (...input) => {
+    let acronimo = '';
+    // Aquí tu código.  Desde aquí:
+    for (i=0; i<input.length; i++){
+      acronimo += input[i][0];
+    }
+    // Hasta aquí.
+    return acronimo;
+}
+
+
+let test = require('../test.js');
+
+test(extraerAcronimo, ['Estudiante', 'Del', 'Bootcamp', 'Fullstack', 'Geekshubs'], 'EDBFG');
+test(extraerAcronimo, ['hola', 'caracola'], 'hc');
+test(extraerAcronimo, ['33', '44',' '], '34 ');
