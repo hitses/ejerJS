fdd444ad4742b867b9defec5ddae91d6d41d7562
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-09 13:46:08 +0200

    Carpeta 7 arreglada

--- 7-Prototypes/1-split.js
+++ 7-Prototypes/1-split.js
@@ -1,10 +1,16 @@
 // Completa la función getCiudadesOrdenada
-// La función recibirá una cadena de texto de ciudades separadas por comas
+// La función recibirá una cadena de texto de ciudades separadas por comas 
+// y devolverá un array de ciudades ordenadas alfabéticamente
 
 // si la función no recibe una cadena de texto o recibe una cadena de texto sin comas debe devolver 'no es un formato correcto'
 
 let getCiudadesOrdenadas= (ciudades) => {
     // Aquí tu código.  Desde aquí:
+    if (ciudades !== 'string'){
+      console.log('no es un formato correcto');
+    } else{
+      return ciudades.split();
+    }
     // Hasta aquí.
 }
 
--- 7-Prototypes/2-reverse.js
+++ 7-Prototypes/2-reverse.js
@@ -1,5 +1,5 @@
 // Un palíndromo es una palabra que se escribe igual del derecho que del revés por ejemplo orejero, rallar o somos.
-// Edita la función querecibirá una cadena de texto y deberá comprar si es un palíndromo o no.
+// Edita la función querecibirá una cadena de texto y deberá devolver si es un palíndromo o no.
 
 // si la función no recibe una cadena de texto o está vacía 'no es un formato correcto'
 
