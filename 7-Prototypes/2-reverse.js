cb8846bcc9a1fdcc7979c1c9f10f9bba6198e5ff
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-09 15:20:03 +0200

    Ejercicio 7-2 hecho

--- 7-Prototypes/2-reverse.js
+++ 7-Prototypes/2-reverse.js
@@ -1,13 +1,23 @@
 // Un palíndromo es una palabra que se escribe igual del derecho que del revés por ejemplo orejero, rallar o somos.
-// Edita la función querecibirá una cadena de texto y deberá devolver si es un palíndromo o no.
+// Edita la función que recibirá una cadena de texto y deberá devolver si es un palíndromo o no.
 
 // si la función no recibe una cadena de texto o está vacía 'no es un formato correcto'
 
 let esPalindromo= (palabra) => {
     // Aquí tu código.  Desde aquí:
+    if (palabra == ''){
+      return 'no es un formato correcto';
+    }else if (typeof palabra !== 'string'){
+      return 'no es un formato correcto';
+    }
+    
+    if (palabra.split('').toString() == palabra.split('').reverse().toString()){
+      return true;
+    } else{
+      return false;
+    }
     //Hasta aqui.
 }
-
 
 let test = require('../test.js');
 
